0130eae1cc42dc3c41b6a19efb3e0f5ea3419912
Francesco Leardini <francesco.leardini@trivadis.com>
2020-06-02 15:29:16 +0200

    Added webServer folder.

---
+++ webServer/server.ts
@@ -0,0 +1,18 @@
+import { Application } from 'https://deno.land/x/oak/mod.ts';
+import { oakCors } from 'https://deno.land/x/cors/mod.ts';
+import router from './src/routes.ts';
+
+const port = 8280;
+const app = new Application();
+
+app.use(oakCors());
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.addEventListener('listen', ({ hostname, port, secure }) => {
+    console.log(`--- Listening on: ${secure ? 'https://' : 'http://'}${
+        hostname ?? 'localhost'
+        }:${port} ---`
+    );
+});
+await app.listen({ port });
---
+++ webServer/src/model/employee.ts
@@ -0,0 +1,14 @@
+export interface Employee {
+    id: number;
+    firstname: string;
+    lastname: string;
+    email?: string;
+}
+
+export const EmployeeData: Employee[] = [
+    { id: 1, firstname: 'Larry', lastname: 'Potter', email: 'max.payne@hotmail.com' },
+    { id: 2, firstname: 'Mara', lastname: 'Croft', email: 'lara.croft@gmail.com' },
+    { id: 3, firstname: 'Thomas', lastname: 'Müller', email: 'thomas123@gmail.com' },
+    { id: 5, firstname: 'Karl', lastname: 'Fritz', email: 'Karl_great@microsoft.com' },
+    { id: 6, firstname: 'Paolo', lastname: 'Rossi' }
+];
